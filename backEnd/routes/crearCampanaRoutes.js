e57bbe73b3f8a233b99006e87d5d61b92ea3e37f
DrewDLeon <andleon.castilleja@gmail.com>
2024-08-23 21:18:49 -0600

    rutas

--- backEnd/routes/crearCampanaRoutes.js
+++ backEnd/routes/crearCampanaRoutes.js
@@ -6,10 +6,13 @@ const crearCampanaController = require('../controllers/crearCampanaController');
 const storage = multer.memoryStorage();
 const upload = multer({storage : storage});
 
-router.get('/getDisponibilidad/:fecha_inicio/:fecha_fin/:daysofweek/:horas', crearCampanaController.getDisponibilidad);
+router.get('/getDisponibilidad/:fecha_inicio/:fecha_fin/:daysofweek/:horas/:id_pantalla', crearCampanaController.getDisponibilidad);
 
 router.post('/crearCampana', upload.single('image'), crearCampanaController.createCampana);
 
 router.post('/testImage', upload.single('image'), crearCampanaController.testImage);
 
 module.exports = router;
+
+
+const {id_usuario, id_pantalla, fecha_inicio, fecha_fin, weekdays, estatus, nombre_campana, costo} = req.body
