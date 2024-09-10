288e93d58e066bc12087bffc0eb97fa346c6eddd
DrewDLeon <andleon.castilleja@gmail.com>
2024-09-10 01:50:31 -0600

    cambio en server.js con la ip del serv

--- backEnd/server.js
+++ backEnd/server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin: 'http://localhost:5173', // Allow only requests from this origin
+  origin: '167.172.145.139', // Allow only requests from this origin
 }));
 
 // Serve static files from the 'uploads' directory
